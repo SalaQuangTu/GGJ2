718a8a3bf037bc01a1cf04de251c86abc882a38e
SalaQuangTu <sala.quangtu@gmail.com>
2020-02-02 09:55:09 +0100

    loose or win fait

--- Assets/Dev/HappynessManager.cs
+++ Assets/Dev/HappynessManager.cs
@@ -16,26 +16,39 @@ public class HappynessManager : MonoBehaviour
 
     public List<float> instrumentsCasse = new List<float>();
 
+    public float finDuGame = 157f;
+    public float timerFinDuGame = 0f;
+
     private void Start()
     {
         happyness = 100;
         youWin.gameObject.SetActive(false);
         youLoose.gameObject.SetActive(false);
+        timerFinDuGame = 0f;
     }
 
     private void Update()
     {
-        if(instrumentsCasse.Count == 0 && happyness < 100)
+        if(timerFinDuGame < finDuGame)
+        {
+            timerFinDuGame += Time.deltaTime;
+        }
+        else
+        {
+            youWin.gameObject.SetActive(true);
+            lvlSelector.LoadNextLevel();
+        }
+
+        if (instrumentsCasse.Count == 0 && happyness < 100)
         {
             happyness += Time.deltaTime;
         }
-        else if(instrumentsCasse.Count > 0 && happyness > 0)
+        else if (instrumentsCasse.Count > 0 && happyness > 0)
         {
             happyness -= Time.deltaTime * instrumentsCasse.Count;
         }
-        else if(happyness <= 0)
+        else if (happyness <= 0)
         {
-            Debug.Log("Bite");
             youLoose.gameObject.SetActive(true);
             lvlSelector.LoadNextLevel();
         }
--- Assets/Dev/Menu.cs
+++ Assets/Dev/Menu.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public LevelSelector lvlSelector;
+    public Text textStart;
 
     public bool onLoad = false;
 
