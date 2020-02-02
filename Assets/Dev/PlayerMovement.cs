a4b174793494d4f0b9894eebf65c90a6179fe082
SalaQuangTu <sala.quangtu@gmail.com>
2020-02-02 16:55:17 +0100

    animControllerJoueur

--- Assets/Dev/PlayerMovement.cs
+++ Assets/Dev/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Animator anim;
+
     public float speed = 12f;
     public float gravity = -9.18f;
     Vector3 velocity;
@@ -16,6 +18,21 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+
+        if(x != 0 || z != 0)
+        {
+            anim.SetBool("Walk", true);
+            anim.SetBool("Repare", false);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        if(Input.GetAxis("Fire1") == 1)
+        {
+            anim.SetBool("Repare", true);
+        }
 
         controller.Move(move * speed * Time.deltaTime);
 
